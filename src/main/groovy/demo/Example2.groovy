06f34aee154ab380159655d35e171092a59bd5a6
Iván López <lopez.ivan@gmail.com>
2017-04-21 19:17:29 +0200

    Example 2

---
+++ src/main/groovy/demo/Example2.groovy
@@ -0,0 +1,15 @@
+package demo
+
+class Example2 {
+
+    boolean isEvenNumber(Integer number) {
+        if (number % 2 == 0)
+            println "The number $number is even"
+        else
+            println "The number $number is odd"
+    }
+
+    void lorem() {
+        println 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis volutpat fermentum enim. Aenean in viverra arcu.'
+    }
+}
